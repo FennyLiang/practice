704b9c041516459acaa428b5ef28a7951635aaef
FennyLiang <shras1993@gmail.com>
2016-09-29 20:53:30 +0800

    add post method

--- to_do_list/todo/app.js
+++ to_do_list/todo/app.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(8080, () => {
   console.log('server is connecting');
@@ -9,4 +11,8 @@ app.listen(8080, () => {
 app.get('/', (req, res) => {
   // res.send('This is get method.');
   res.sendFile( __dirname + '/index.html'); // __dirname 指向當前的資料夾位置
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
 })
