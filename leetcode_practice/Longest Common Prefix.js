31fe0623413926044292981e24fad883513e062b
FennyLiang <shras1993@gmail.com>
2016-10-02 13:33:14 +0800

    add problem explanation.

--- leetcode_practice/Longest Common Prefix.js
+++ leetcode_practice/Longest Common Prefix.js
@@ -2,9 +2,16 @@
  * @param {string[]} strs
  * @return {string}
  * Write a function to find the longest common prefix string amongst an array of strings.
- */
+ * 一個陣列中有許多個字串，寫一個function找出這些字串最長的共同字首。
+ 範例：
+ ['abcd','abccc','abdec'] --> 共同字首為 'ab' 。
 
-strs = ['11222', '111223', '111122233'];
+ *思路
+ *比對前兩個字串，從頭開始取出相同的部分為共同字首
+ *後面的字串只要與目前的共同字首比對即可
+ *['abcd','abccc','abdec'] ，一開始'abcd','abccc'共同字首前3碼'abc'
+ *接下來只要將'abc','abdec'做比對，發現剩下'ab'，也就是最長的共同字首
+ */
 
 var longestCommonPrefix = function(strs) {
   if(strs.length === 0 || strs === null){
