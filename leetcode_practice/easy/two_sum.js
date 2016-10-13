fc8aba9ee51a4680aa7058964e0db2557386ea8f
FennyLiang <shras1993@gmail.com>
2016-10-13 22:21:46 +0800

    use node process two_sum

--- leetcode_practice/easy/two_sum.js
+++ leetcode_practice/easy/two_sum.js
@@ -8,10 +8,16 @@ var twoSum = function (nums, target){
   for(var i = 0; i < nums.length ; i++ ){
     for(var j = i+1 ; j < nums.length ; j++){
       if(nums[i] + nums[j] == target)
-        return [i,j];
+        return [i, j];
     }
   }
 };
+
+var nums = [1, 2, 3, 4, 5];
+var target = 9;
+var value = twoSum(nums, target);
+
+console.log(`nums[i, j] Address:::: ${value}`)
 
 // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 //
