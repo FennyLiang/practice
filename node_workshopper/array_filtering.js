cd44c9d558c90f8d91989fb542804bad1c07cf2d
FennyLiang <shras1993@gmail.com>
2016-11-11 16:52:05 +0800

    change comment

--- node_workshopper/array_filtering.js
+++ node_workshopper/array_filtering.js
@@ -1,7 +1,7 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 var filtered = numbers.filter( (number) => {
-          return (number % 2) === 0  //印出的是已過濾的陣列數值
+          return (number % 2) === 0  //filter特性印出的是已過濾的陣列數值
   }
 );
 
