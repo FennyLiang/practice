f63a68efab9dc15531de4ec35b2313aeed4aca57
FennyLiang <shras1993@gmail.com>
2016-11-16 12:32:49 +0800

    add replace method solution

---
+++ node_workshopper/replace_String.js
@@ -0,0 +1,10 @@
+// 使用 .replace() 方法
+// var example = 'this example exists';
+// example = example.replace('exists', 'is awesome');
+
+
+var pizza = "pizza is alright"
+
+var pizzasaid = pizza.replace("alright", "wonderful")
+
+console.log(pizzasaid);
