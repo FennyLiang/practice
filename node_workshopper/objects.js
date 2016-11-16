5191d2979e30afb9926a88ba566d257a8dca72e8
FennyLiang <shras1993@gmail.com>
2016-11-16 11:56:49 +0800

    add objects practice solution

---
+++ node_workshopper/objects.js
@@ -0,0 +1,10 @@
+var pizza = {
+
+  toppings : ["pineapple", "Cheese", "pepperoni"],
+  crust : "deep dish",
+  serves : 2
+
+}
+
+console.log(pizza)
+console.log(`Let toppings value to String::::${pizza['toppings'].toString()}`)
