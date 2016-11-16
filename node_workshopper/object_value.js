fba636180ba9b5b51a5b5607e02bf7e85ceb9689
FennyLiang <shras1993@gmail.com>
2016-11-16 11:34:17 +0800

    add solution

--- node_workshopper/object_value.js
+++ node_workshopper/object_value.js
@@ -1,3 +1,15 @@
 //使用與存取和操作陣列非常類似的方法來存取和操作物件的屬性——屬性就是物件所包含的鍵和值的對
+// var example = {
+// pizza: 'yummy'
+// };
 //example.pizza;
 // example['pizza']; => 兩者皆可取得物件的值
+
+var food = {
+
+  types : "only pizza"
+
+}
+
+console.log(food.types)
+
