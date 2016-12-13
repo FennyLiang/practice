9c23901e2fbfcd617a0942e66d7fdb8669933e0a
FennyLiang <shras1993@gmail.com>
2016-12-13 15:02:27 +0800

    modify push method

--- notification/test-fcm-sccess.js
+++ notification/test-fcm-sccess.js
@@ -1,17 +1,15 @@
 var fetch = require('node-fetch');
 var util = require('util');
 
+var API_KEY = ""; //FCMcredentials Key
 var deliverData = {
   notification: {
-    title: 'WTF with FCM....',
-    body: 'Damn!!!!',
+    title: 'WeMo',
+    body: 'notification test', //process.argv[2]
   },
+  to : '' // user token
 
-  // This is raw token from device but WTF sender is doesn't match !!!!!
-  // cbfjBBu-e80:APA91bGH62YzsJDwEzIxBKrG4tsNtqFkoph4IdoDFRWntWvoIzKZy92P0am2HdGRPwuAFUpRfXBv9uJKIZIsFHZFRRNe_iB14AVoabGvTBJCb1Rjn0s5fQnhqxi4BlAWfNNOL37z-h22
-
-  // registration_ids: [AAAA6H8fs-w:APA91bGH62YzsJDwEzIxBKrG4tsNtqFkoph4IdoDFRWntWvoIzKZy92P0am2HdGRPwuAFUpRfXBv9uJKIZIsFHZFRRNe_iB14AVoabGvTBJCb1Rjn0s5fQnhqxi4BlAWfNNOL37z-h22']
-  condition: "'AllUser' in topics",
+  // condition: "'AllUser' in topics", => 此為推播給所有用戶
 }
 
 console.log(JSON.stringify(deliverData));
@@ -19,11 +17,11 @@ console.log(JSON.stringify(deliverData));
 fetch('https://fcm.googleapis.com/fcm/send', {
   method: 'POST',
   headers: {
-    Authorization: 'key=AAAA6H8fs-w:APA91bELFf3XFMDPmMigsZT_vfsFfCWzRG13CrKudX6hLkDDdPNsT4FPfOEImGM19RCFYVELyUO9ySnS9XI8snBM7NbE7s_cMsWoijY1TNbeJ_PmLNTAVmf8x45IYNhBLCxWJkE-Mv2pifkgLKsSEV9oj5UNL8tGxA',
+    'Authorization': 'key='+API_KEY,
     'Content-Type': 'application/json',
   },
   body: JSON.stringify(deliverData),
-  credentials: 'include',
+  // credentials: 'include',
 })
   .then(function(response) {
     return response.json();
